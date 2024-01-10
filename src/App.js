7a887acba37af214718603415f029c946ae10b5c
Rajendrasinh Rathod <rajendra.rtdl@gmail.com>
2024-01-10 18:54:50 +0530

    Added CI/CD

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          AWS HOSTING
+          AWS HOSTING BY RAJENDRA SINH RATHOD
         </a>
       </header>
     </div>
